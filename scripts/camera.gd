9aa7a1dd091a024a2bfd272d558d0529b7429b2e
CanNotCode2 <156641675+CanNotCode2@users.noreply.github.com>
2024-03-30 18:42:20 -0400

    Fix camera camera/map navigation and zoom

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,37 +1,45 @@
 extends Camera2D
 
-var ZoomMin = Vector2(0.25,0.25)
-var ZoomMax = Vector2(2.5,2.5)
-var ZoomSpd = Vector2(0.3,0.3)
-var PanSpeedKey = 8
+var ZOOM_MIN: Vector2   = Vector2(0.25,0.25)
+var ZOOM_MAX: Vector2   = Vector2(2.5,2.5)
+var zoom_speed: Vector2 = Vector2(0.3,0.3)
+var PanSpeedKey: int    = 8
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-func _input(event):
-	if event.is_action_pressed("ScrollZoomOut"):
-		if zoom > ZoomMin:
-			zoom -= ZoomSpd
-		print("Zoom out")
-	if event.is_action_pressed("ScrollZoomIn"):
-		if zoom < ZoomMax:
-			zoom +=ZoomSpd
-		print("Zoom in")
-	print(zoom)
+# Called on input independent of frame times
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			if zoom >= ZOOM_MIN:
+				zoom -= zoom_speed
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			if zoom <= ZOOM_MAX:
+				zoom += zoom_speed
+
+	#if event is InputEventMouseButton:
+
+	#if event.is_action_pressed("zoom_out"):
+		#if zoom > ZoomMin:
+			#zoom -= ZoomSpd
+		#print("Zoom out")
+	#if event.is_action_pressed("zoom_in"):
+		#if zoom < ZoomMax:
+			#zoom +=ZoomSpd
+		#print("Zoom in")
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if Input.is_action_pressed("MoveCamUp"):
+func _process(_delta) -> void:
+	# This code works for pressing multiple keys at the same time
+	if Input.is_action_pressed("ui_up"):
 		position.y -= PanSpeedKey
-		print("MoveCamUp")
-	if Input.is_action_pressed("MoveCamDown"):
+	if Input.is_action_pressed("ui_down"):
 		position.y += PanSpeedKey
-		print("MoveCamDown")
-	if Input.is_action_pressed("MoveCamLeft"):
-		print("MoveCamLeft")
+	if Input.is_action_pressed("ui_left"):
 		position.x -= PanSpeedKey
-	if Input.is_action_pressed("MoveCamRight"):
+	if Input.is_action_pressed("ui_right"):
 		position.x += PanSpeedKey
-		print("MoveCamRight")
+	# print(position)
 	pass
