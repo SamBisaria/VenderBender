381978aebfd09edfa4ce0e8ce69a5ae3b0a02b9b
spbisaria <sbisaria@unc.edu>
2024-03-30 22:21:02 -0400

    Smooth zoom and audio boilerplate

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,27 +1,26 @@
 extends Camera2D
 
 var PanSpeedKey: int = 8
-
-# Zoom speed
-var zoom_speed := 0.12
-
-# Minimum and maximum zoom levels
+var zoom_speed := 0.1
+var zoom_smoothness := 5.0
 var min_zoom := 0.2
 var max_zoom := 2.0
+var target_zoom := Vector2(1.0, 1.0)
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			zoom += Vector2(zoom_speed, zoom_speed)
+			target_zoom += Vector2(zoom_speed, zoom_speed)
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			zoom -= Vector2(zoom_speed, zoom_speed)
-		zoom = Vector2(
-			clamp(zoom.x, min_zoom, max_zoom),
-			clamp(zoom.y, min_zoom, max_zoom)
+			target_zoom -= Vector2(zoom_speed, zoom_speed)
+		
+		target_zoom = Vector2(
+			clamp(target_zoom.x, min_zoom, max_zoom),
+			clamp(target_zoom.y, min_zoom, max_zoom)
 		)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta) -> void:
+func _process(delta):
+	zoom = lerp(zoom, target_zoom, delta * zoom_smoothness)
 	if Input.is_action_pressed("ui_up"):
 		position.y -= PanSpeedKey
 	if Input.is_action_pressed("ui_down"):
@@ -30,4 +29,3 @@ func _process(delta) -> void:
 		position.x -= PanSpeedKey
 	if Input.is_action_pressed("ui_right"):
 		position.x += PanSpeedKey
-
