2983648709d827b5c86689c79d6c5f8c9443bad3
spbisaria <sbisaria@unc.edu>
2024-03-30 21:22:56 -0400

    Fix zoom feature

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,44 +1,27 @@
 extends Camera2D
 
-var PanSpeedKey: int    = 8
+var PanSpeedKey: int = 8
 
-var zoomspeed: int    = 100
-var zoommargin: float = 0.3
+# Zoom speed
+var zoom_speed := 0.12
 
-var zoomMin: float = 0.2
-var zoomMax: int   = 1
+# Minimum and maximum zoom levels
+var min_zoom := 0.2
+var max_zoom := 2.0
 
-var zoompos: Vector2  = Vector2()
-var zoomfactor: float = 1.0
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-# Called on input independent of frame times
-func _input(event: InputEvent) -> void:
-	if (abs(zoompos.x - get_global_mouse_position().x) > zoommargin):
-		zoomfactor = 1.0
-	if (abs(zoompos.y - get_global_mouse_position().y) > zoommargin):
-		zoomfactor = 1.0
-	
+func _input(event):
 	if event is InputEventMouseButton:
-		if event.is_pressed():
-			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-				zoomfactor -= 0.01
-				zoompos = get_global_mouse_position()
-			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-				zoomfactor += 0.01
-				zoompos = get_global_mouse_position()
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			zoom += Vector2(zoom_speed, zoom_speed)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			zoom -= Vector2(zoom_speed, zoom_speed)
+		zoom = Vector2(
+			clamp(zoom.x, min_zoom, max_zoom),
+			clamp(zoom.y, min_zoom, max_zoom)
+		)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
-	zoom.x = lerp(zoom.x, zoom.x * zoomfactor, zoomspeed * delta)
-	zoom.y = lerp(zoom.y, zoom.y * zoomfactor, zoomspeed * delta)
-	zoom.x = clamp(zoom.x, zoomMin, zoomMax)
-	zoom.y = clamp(zoom.y, zoomMin, zoomMax)
-	
-	# This code works for pressing multiple keys at the same time
 	if Input.is_action_pressed("ui_up"):
 		position.y -= PanSpeedKey
 	if Input.is_action_pressed("ui_down"):
@@ -47,3 +30,4 @@ func _process(delta) -> void:
 		position.x -= PanSpeedKey
 	if Input.is_action_pressed("ui_right"):
 		position.x += PanSpeedKey
+
